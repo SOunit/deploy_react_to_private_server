b75746497a8bc48aa6230d4ff595ed6e0ef2813b
SOunit <workbookwork7@gmail.com>
2021-10-21 08:34:59 -0700

    add posting php api logic

--- react_subdir_deploy/src/pages/post.js
+++ react_subdir_deploy/src/pages/post.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
 import axios from 'axios';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './post.module.css';
 
 const Post = () => {
+  const [nameInput, setNameInput] = useState('Jack');
   const [message, setMessage] = useState('');
 
   const submitHandler = (e) => {
@@ -10,11 +12,28 @@ const Post = () => {
     console.log('submit');
 
     // get to json placeholder
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((data) => {
-      console.log(data);
-    });
+    // axios.get('https://jsonplaceholder.typicode.com/todos/1').then((data) => {
+    //   console.log(data);
+    // });
 
-    setMessage('submit!!');
+    // axios
+    //   .get('http://yurtyurt.com/php/get/json/')
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((err) => console.log(err));
+
+    const obj = { name: nameInput };
+    const jsonObj = JSON.stringify(obj);
+    console.log(jsonObj);
+
+    axios
+      .post('http://yurtyurt.com/php/post/json/', jsonObj)
+      .then((data) => {
+        console.log(data.data.greeting);
+        setMessage(data.data.greeting);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -23,9 +42,17 @@ const Post = () => {
         <label className={classes['form__label']} htmlFor='name'>
           Name
         </label>
-        <input className={classes['form__input']} id='name' />
+        <input
+          className={classes['form__input']}
+          id='name'
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
         <button className={classes['form__button']}>Submit</button>
       </form>
+      <Link to='/first' className={classes['link']}>
+        To First
+      </Link>
       <div className={classes['result']}>{message}</div>
     </div>
   );
--- react_subdir_deploy/src/pages/second.js
+++ react_subdir_deploy/src/pages/second.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import LinkButton from '../component/linkButton/LinkButton';
 import classes from './second.module.css';
 
 const Second = () => {
   return (
     <div className={classes['second']}>
       <h1 className={classes['second__title']}>Second</h1>
-      <Link to='/first'>To First</Link>
-      <Link to='/post'>To POST</Link>
+      <LinkButton to='/first' text='To First'></LinkButton>
+      <LinkButton to='/post' text='To Post'></LinkButton>
     </div>
   );
 };
